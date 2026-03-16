b07a8529d48b17e384e7bed768b535bc13b27aa2
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2026-03-16 16:10:45 -0300

    aula 3 e 4
    Aulas 3 e 4 concluidas

---
+++ UNIDADE2-Estruturas-do-Desenvolvimento-Orientado-a-Objetos/Heranca/HerancaInterface.java
@@ -0,0 +1,8 @@
+public class HerancaInterface {
+
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        cachorro.falar(); // Saída: O cachorro late.
+        cachorro.excutarComando("Sentar"); // Saída: O cachorro executa o comando: Sentar
+    }
+}
