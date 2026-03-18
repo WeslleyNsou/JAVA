bce84e82295eb3604eefd45854402ae35d837a64
Weslley souza <120567284+WeslleyNsou@users.noreply.github.com>
2026-03-18 09:26:30 -0300

    Unidade3/ Aula1
    Atividade da Unidade 3/ Aula 1

---
+++ Aluno.java
@@ -0,0 +1,40 @@
+public class Aluno {
+    private String nome;
+    private int matricula;
+    private double nota;
+
+    public Aluno(String nome, int matricula, double nota) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        if (nota >= 0 && nota <= 10) {
+            this.nota = nota;
+        } else {
+            System.out.println("Nota inválida. A nota deve ser entre 0 e 10.");
+        }
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Nota: " + nota);
+    }
+}
---
+++ Escola.java
@@ -0,0 +1,6 @@
+public class Escola {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("João", 12345, 11.5);
+        aluno1.exibirInformacoes();
+    }
+}
